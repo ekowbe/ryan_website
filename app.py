59d0a5d7a217a1eea36bf58bf1ec0386c6e6a49f
ekowbe <74758193+ekowbe@users.noreply.github.com>
2022-11-17 10:23:06 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from time import strftime
+from flask import Flask, request, make_response, redirect, url_for
+from flask import render_template
+from datetime import datetime
+
+
+#-----------------------------------------------------------------------
+
+app = Flask(__name__, template_folder='.')
+
+#-----------------------------------------------------------------------
+
+
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def index():    
+
+
+
+    html = render_template('index.html')
+    response = make_response(html)
+    return response
---
+++ runserver.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sqlite3
+from sys import argv, exit, stderr
+from cases_app import app
+
+def main():
+    
+    if len(argv) != 2:
+        print('Usage: ' + argv[0] + ' port', file=stderr)
+        exit(1)
+
+    try:
+        port = int(argv[1])
+    except Exception:
+        print('Port must be an integer.', file=stderr)
+        exit(1)
+
+    try:
+        app.run(host='0.0.0.0', port=port, debug=True)
+    except Exception as ex:
+        print(ex, file=stderr)
+        exit(1)
+
+if __name__ == '__main__':
+    main()
